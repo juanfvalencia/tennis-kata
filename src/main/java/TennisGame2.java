b49d307dbe805d8844f56866e9cae87f5f52f69b
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-19 20:29:09 -0500

    Se realiza extracción de metodos para minimizar el uso de condicionales en Game2

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -16,104 +16,70 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (p1Point == p2Point && p1Point < 4)
-        {
-            if (p1Point == 0)
-                score = "Love";
-            if (p1Point == 1)
-                score = "Fifteen";
-            if (p1Point == 2)
-                score = "Thirty";
-            score += "-All";
+        if (equalsScore()){
+            score = equalScore(score);
         }
-        if (p1Point==p2Point && p1Point>=3)
-            score = "Deuce";
-
-        if (p1Point > 0 && p2Point==0)
+        else if (scorePlayers())
         {
-            if (p1Point==1)
-                p1Res = "Fifteen";
-            if (p1Point==2)
-                p1Res = "Thirty";
-            if (p1Point==3)
-                p1Res = "Forty";
-
-            p2Res = "Love";
-            score = p1Res + "-" + p2Res;
+            score = scoreGreaterThan4();
         }
-        if (p2Point > 0 && p1Point==0)
+        else
         {
-            if (p2Point==1)
-                p2Res = "Fifteen";
-            if (p2Point==2)
-                p2Res = "Thirty";
-            if (p2Point==3)
-                p2Res = "Forty";
-
-            p1Res = "Love";
-            score = p1Res + "-" + p2Res;
+            score = differentScore(p1Point) + "-" + differentScore(p2Point);
         }
 
-        if (p1Point>p2Point && p1Point < 4)
-        {
-            if (p1Point==2)
-                p1Res="Thirty";
-            if (p1Point==3)
-                p1Res="Forty";
-            if (p2Point==1)
-                p2Res="Fifteen";
-            if (p2Point==2)
-                p2Res="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point>p1Point && p2Point < 4)
-        {
-            if (p2Point==2)
-                p2Res="Thirty";
-            if (p2Point==3)
-                p2Res="Forty";
-            if (p1Point==1)
-                p1Res="Fifteen";
-            if (p1Point==2)
-                p1Res="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
-        
-        if (p1Point > p2Point && p2Point >= 3)
-        {
-            score = "Advantage player1";
-        }
-
-        if (p2Point > p1Point && p1Point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (p1Point>=4 && p2Point>=0 && (p1Point-p2Point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (p2Point>=4 && p1Point>=0 && (p2Point-p1Point)>=2)
-        {
-            score = "Win for player2";
-        }
         return score;
     }
-    
 
-    
-    public void P1Score(){
-        p1Point++;
+    private boolean scorePlayers() {
+        return p1Point >=4 || p2Point >=4;
     }
-    
-    public void P2Score(){
-        p2Point++;
+
+    private boolean equalsScore() {
+        return p1Point == p2Point;
     }
+
+    private String equalScore(String score) {
+        switch (p1Point){
+            case 0: return "Love-All";
+            case 1: return "Fifteen-All";
+            case 2: return "Thirty-All";
+            default: return "Deuce";
+        }
+    }
+
 
     public void wonPoint(String player) {
         if (player == "player1")
-            P1Score();
+            p1Point++;
         else
-            P2Score();
+            p2Point++;
+    }
+
+    private String scoreGreaterThan4() {
+        int minusResult = p1Point - p2Point;
+        switch(minusResult){
+            case 1: case -1: return advantageScore(minusResult);
+            default: return winScore(minusResult);
+        }
+    }
+
+    private String advantageScore(int minusResult){
+        return minusResult == 1 ? "Advantage player1" : "Advantage player2";
+
+    }
+
+    private String winScore(int minusResult){
+        return minusResult >= 2 ? "Win for player1": "Win for player2";
+    }
+
+    private String differentScore(int scorePlayer) {
+        switch(scorePlayer)
+        {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            default: return "Forty";
+        }
     }
 }
