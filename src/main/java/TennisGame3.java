4e0ca1787472b552112265ba42d5d34f5148eba9
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-19 20:34:52 -0500

    Se refactoriza con extracción de metodo lowerThan4, scoreIsLowerThan4, additionDifferentTo6 y bothScoreLowerThan4

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -3,20 +3,18 @@ public class TennisGame3 implements TennisGame {
 
     private int scorePlayer2;
     private int scorePlayer1;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.player1Name = p1N;
-        this.player2Name = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
         String s;
-        if (scorePlayer1 < 4 && scorePlayer2 < 4 && !(scorePlayer1 + scorePlayer2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[scorePlayer1];
-            return (scorePlayer1 == scorePlayer2) ? s + "-All" : s + "-" + p[scorePlayer2];
+        if (scoreIsLowerThan4()) {
+            return lowerThan4();
         } else {
             if (scorePlayer1 == scorePlayer2)
                 return "Deuce";
@@ -24,13 +22,32 @@ public class TennisGame3 implements TennisGame {
             return ((scorePlayer1 - scorePlayer2)*(scorePlayer1 - scorePlayer2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
+
+    private String lowerThan4() {
+        String s;
+        String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        s = p[scorePlayer1];
+        return (scorePlayer1 == scorePlayer2) ? s + "-All" : s + "-" + p[scorePlayer2];
+    }
     
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             this.scorePlayer1++;
         else
             this.scorePlayer2++;
         
+    }
+
+    private boolean scoreIsLowerThan4() {
+        return bothScoreLowerThan4() && additionDifferentTo6();
+    }
+
+    private boolean additionDifferentTo6() {
+        return scorePlayer1 + scorePlayer2 != 6;
+    }
+
+    private boolean bothScoreLowerThan4() {
+        return scorePlayer1 < 4 && scorePlayer2 < 4;
     }
 
 }
