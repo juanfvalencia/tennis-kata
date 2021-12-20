20ec0a30a69d635d5ba718a683b84d3fb4f5a9a7
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-19 20:07:32 -0500

    Se refactrizan los case con retorno directo

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -43,17 +43,13 @@ public class TennisGame1 implements TennisGame {
                 switch(tempScore)
                 {
                     case 0:
-                        score += "Love";
-                        break;
+                        return "Love";
                     case 1:
-                        score += "Fifteen";
-                        break;
+                        return "Fifteen";
                     case 2:
-                        score += "Thirty";
-                        break;
+                        return "Thirty";
                     case 3:
-                        score += "Forty";
-                        break;
+                        return "Forty";
                 }
             }
         }
@@ -64,19 +60,14 @@ public class TennisGame1 implements TennisGame {
         switch (scorePlayer1)
         {
             case 0:
-                score = "Love-All";
-                break;
+                return "Love-All";
             case 1:
-                score = "Fifteen-All";
-                break;
+                return "Fifteen-All";
             case 2:
-                score = "Thirty-All";
-                break;
+                return "Thirty-All";
             default:
-                score = "Deuce";
-                break;
+                return "Deuce";
 
         }
-        return score;
     }
 }
