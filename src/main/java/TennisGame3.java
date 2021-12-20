13fb0a52775a2dc4ed1290b222b7a189789944f0
juanfvalencia <90592176+juanfvalencia@users.noreply.github.com>
2021-12-19 20:31:23 -0500

    Se refactorizan las variables de Game3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,35 +1,35 @@
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+
+    private int scorePlayer2;
+    private int scorePlayer1;
+    private String player1Name;
+    private String player2Name;
 
     public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+        this.player1Name = p1N;
+        this.player2Name = p2N;
     }
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (scorePlayer1 < 4 && scorePlayer2 < 4 && !(scorePlayer1 + scorePlayer2 == 6)) {
+            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+            s = p[scorePlayer1];
+            return (scorePlayer1 == scorePlayer2) ? s + "-All" : s + "-" + p[scorePlayer2];
         } else {
-            if (p1 == p2)
+            if (scorePlayer1 == scorePlayer2)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = scorePlayer1 > scorePlayer2 ? player1Name : player2Name;
+            return ((scorePlayer1 - scorePlayer2)*(scorePlayer1 - scorePlayer2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
     
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.scorePlayer1++;
         else
-            this.p2 += 1;
+            this.scorePlayer2++;
         
     }
 
